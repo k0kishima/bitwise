09ef13d7336fd6762bd6bc32e5dd93d7729dd174
k0kishima <56298669+k0kishima@users.noreply.github.com>
2024-08-31 08:27:15 +0900

    Add a theme

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,36 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    const baseTextStyle = TextStyle(color: Colors.black54);
+
+    return MaterialApp(
       localizationsDelegates: L10n.localizationsDelegates,
       supportedLocales: L10n.supportedLocales,
-      locale: Locale('ja', ''),
-      home: Screen(),
+      locale: const Locale('ja', ''),
+      theme: ThemeData(
+        primaryColor: Colors.black87,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.black87,
+          foregroundColor: Colors.white,
+        ),
+        scaffoldBackgroundColor: Colors.white,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.grey.shade600,
+            side: BorderSide(color: Colors.grey.shade400, width: 1),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+        ),
+        textTheme: const TextTheme(
+          bodyLarge: baseTextStyle,
+          bodyMedium: baseTextStyle,
+          bodySmall: baseTextStyle,
+        ),
+      ),
+      home: const Screen(),
     );
   }
 }
--- lib/src/features/home/presentation/screen.dart
+++ lib/src/features/home/presentation/screen.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:intl/intl.dart';
 
 class Screen extends StatelessWidget {
   const Screen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Intl.defaultLocale = Localizations.localeOf(context).languageCode;
     final l10n = L10n.of(context);
+    final theme = Theme.of(context);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(l10n.appName),
-        backgroundColor: Colors.black87,
-        foregroundColor: Colors.white,
       ),
-      backgroundColor: Colors.white,
       body: Center(
-        child: Text(l10n.greeting),
+        child: Text(
+          l10n.greeting,
+          style: theme.textTheme.bodyLarge,
+        ),
       ),
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -29,20 +28,7 @@ class Screen extends StatelessWidget {
             onPressed: () {
               // Add your play button logic here
             },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.white,
-              foregroundColor: Colors.white,
-              side: BorderSide(color: Colors.grey.shade400, width: 1),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15),
-              ),
-            ),
-            child: Text(
-              l10n.play,
-              style: TextStyle(
-                color: Colors.grey.shade600,
-              ),
-            ),
+            child: Text(l10n.play),
           ),
         ),
       ),
