3e40f6799d43890c3fe015ed4f4e22ae2b8b8f3f
k0kishima <56298669+k0kishima@users.noreply.github.com>
2024-08-31 12:33:46 +0900

    Set a limit on the number of questions

---
+++ lib/src/features/end/presentation/screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import '../../play/presentation/screen.dart';
+
+class EndScreen extends StatelessWidget {
+  const EndScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle buttonStyle = ElevatedButton.styleFrom(
+      backgroundColor: Colors.white,
+      foregroundColor: Colors.grey.shade600,
+      side: BorderSide(color: Colors.grey.shade400, width: 1),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Game Over'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Congratulations!',
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: buttonStyle,
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    child: Text('Home'),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => const PlayScreen()),
+                    );
+                  },
+                  style: buttonStyle,
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    child: Text('Replay'),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/src/features/play/presentation/screen.dart
+++ lib/src/features/play/presentation/screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../end/presentation/screen.dart';
 import 'dart:math';
 
 class PlayScreen extends StatefulWidget {
@@ -12,6 +13,8 @@ class _PlayScreenState extends State<PlayScreen> {
   int targetValue = 0;
   List<String> values = List.filled(8, '0');
   bool correct = false;
+  int correctAnswers = 0;
+  final int totalQuestions = 10;
 
   @override
   void initState() {
@@ -31,13 +34,23 @@ class _PlayScreenState extends State<PlayScreen> {
       int decimalValue = int.parse(values.join(), radix: 2);
       if (decimalValue == targetValue) {
         correct = true;
-        Future.delayed(const Duration(milliseconds: 500), () {
-          setState(() {
-            correct = false;
-            _generateTargetValue();
-            values = List.filled(8, '0');
+        correctAnswers += 1;
+        if (correctAnswers >= totalQuestions) {
+          Future.delayed(const Duration(milliseconds: 500), () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const EndScreen()),
+            );
           });
-        });
+        } else {
+          Future.delayed(const Duration(milliseconds: 500), () {
+            setState(() {
+              correct = false;
+              _generateTargetValue();
+              values = List.filled(8, '0');
+            });
+          });
+        }
       }
     });
   }
@@ -48,61 +61,76 @@ class _PlayScreenState extends State<PlayScreen> {
       appBar: AppBar(),
       body: Stack(
         children: [
-          if (!correct)
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    targetValue.toString(),
-                    style: const TextStyle(fontSize: 24),
-                  ),
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: values.asMap().entries.map((entry) {
-                      int index = entry.key;
-                      String value = entry.value;
-                      return GestureDetector(
-                        onTap: () => _toggleValue(index),
-                        child: Container(
-                          width: 40,
-                          height: 40,
-                          alignment: Alignment.center,
-                          margin: const EdgeInsets.symmetric(horizontal: 5),
-                          color: Colors.grey.shade200,
-                          child: Text(
-                            value,
-                            style: const TextStyle(fontSize: 24),
+          Column(
+            children: [
+              LinearProgressIndicator(
+                value: correctAnswers / totalQuestions,
+                backgroundColor: Colors.grey.shade300,
+                color: Colors.grey.shade600,
+              ),
+              Expanded(
+                child: Stack(
+                  children: [
+                    if (!correct)
+                      Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              targetValue.toString(),
+                              style: const TextStyle(fontSize: 24),
+                            ),
+                            const SizedBox(height: 20),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: values.asMap().entries.map((entry) {
+                                int index = entry.key;
+                                String value = entry.value;
+                                return GestureDetector(
+                                  onTap: () => _toggleValue(index),
+                                  child: Container(
+                                    width: 40,
+                                    height: 40,
+                                    alignment: Alignment.center,
+                                    margin: const EdgeInsets.symmetric(horizontal: 5),
+                                    color: Colors.grey.shade200,
+                                    child: Text(
+                                      value,
+                                      style: const TextStyle(fontSize: 24),
+                                    ),
+                                  ),
+                                );
+                              }).toList(),
+                            ),
+                          ],
+                        ),
+                      ),
+                    if (correct)
+                      Positioned.fill(
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(30.0),
+                            child: Container(
+                              width: MediaQuery.of(context).size.width * 0.8,
+                              height: MediaQuery.of(context).size.width * 0.8,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.green.withOpacity(0.9),
+                              ),
+                              child: const Icon(
+                                Icons.check,
+                                size: 100,
+                                color: Colors.white,
+                              ),
+                            ),
                           ),
                         ),
-                      );
-                    }).toList(),
-                  ),
-                ],
-              ),
-            ),
-          if (correct)
-            Positioned.fill(
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: Container(
-                    width: MediaQuery.of(context).size.width * 0.8,
-                    height: MediaQuery.of(context).size.width * 0.8,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.green.withOpacity(0.9),
-                    ),
-                    child: const Icon(
-                      Icons.check,
-                      size: 100,
-                      color: Colors.white,
-                    ),
-                  ),
+                      ),
+                  ],
                 ),
               ),
-            ),
+            ],
+          ),
         ],
       ),
     );
